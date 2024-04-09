4f8b75bef813b5b2c5a1e02215019cd5a3218418
Ricardo Chavez <74266998+ExHultant@users.noreply.github.com>
2024-04-09 03:04:04 -0400

    Add google_fonts dependency to pubspec.yaml

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
 import 'package:login/menu.dart' as menu;
 import 'package:login/registro.dart' as registro;
-final supabase = Supabase.instance.client;
 
+final supabase = Supabase.instance.client;
 
 Future<void> main() async {
   await Supabase.initialize(
     url: 'https://qndkjfhorupharaphaoa.supabase.co',
-    anonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFuZGtqZmhvcnVwaGFyYXBoYW9hIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTI1MDQwMjEsImV4cCI6MjAyODA4MDAyMX0.Fok8bU8EcKUZig0MJZjxQZxOf6ZWnWvjwbzwmRvBU-E',
+    anonKey:
+        'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFuZGtqZmhvcnVwaGFyYXBoYW9hIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTI1MDQwMjEsImV4cCI6MjAyODA4MDAyMX0.Fok8bU8EcKUZig0MJZjxQZxOf6ZWnWvjwbzwmRvBU-E',
   );
 
   runApp(const MainApp());
@@ -19,11 +21,12 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return  MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(centerTitle: true, title: const Text('Login'),),
-        body: const Login()
+    return MaterialApp(
+      theme: ThemeData(
+        fontFamily: GoogleFonts.montserrat().fontFamily,
+        scaffoldBackgroundColor: Colors.deepPurple,
       ),
+      home: const Login(),
     );
   }
 }
@@ -36,14 +39,13 @@ class Login extends StatefulWidget {
 }
 
 class _LoginState extends State<Login> {
-
   final emailcontrol = TextEditingController();
   final contracontrol = TextEditingController();
-  
-@override
+
+  @override
   void initState() {
     super.initState();
-    
+
     Supabase.instance.client
         .channel('contraseñas')
         .onPostgresChanges(
@@ -52,48 +54,156 @@ class _LoginState extends State<Login> {
             table: 'contraseñas',
             callback: (payload) {
               debugPrint('');
-             
             })
         .subscribe();
   }
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-     child:
-      Column(
-      children: [
-        const SizedBox(height: 50,),
-        SizedBox(
-          width: 350,
-        child:TextField(
-          controller: emailcontrol,
-          decoration: const InputDecoration(labelText: 'Correo',border: OutlineInputBorder()),
-          keyboardType: TextInputType.emailAddress,
-        )),
-        const SizedBox(height: 30,),
-        SizedBox(
-          width: 350,
-        child: TextField(
-          controller: contracontrol,
-          decoration: const InputDecoration(labelText: 'Contraseña',border: OutlineInputBorder()),
-          obscureText: true,
-        )),
-        const SizedBox(height: 40,),
-        MaterialButton(onPressed: () async {
-          //final sesion =
-          //supabase.auth.signUp(password: contracontrol.text,email: emailcontrol.text);
-          //final AuthResponse res = 
-          /*await supabase.auth.signUp(
-          email: emailcontrol.text,
-          password: contracontrol.text);*/
-          Navigator.push(context, MaterialPageRoute(builder: (context) => const menu.Menu()));
-        }, child: const Text('Ingresar'),),
-        const SizedBox(height: 20,),
-        MaterialButton(onPressed: () async {
-          Navigator.push(context, MaterialPageRoute(builder: (context) => const registro.Registro()));
-        }, child: const Text('Registrarte'),),
-      ],
-    ));
+    return Scaffold(
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              const Icon(
+                Icons.account_circle,
+                size: 100,
+                color: Colors.white,
+              ),
+              const Text(
+                'Iniciar Sesión',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 24,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              const Text(
+                'Ingresa tus datos para continuar',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 16,
+                ),
+              ),
+              const SizedBox(
+                height: 40,
+              ),
+              SizedBox(
+                width: double.infinity,
+                child: TextField(
+                  controller: emailcontrol,
+                  decoration: const InputDecoration(
+                    hintText: 'Correo electrónico',
+                    border: OutlineInputBorder(
+                      borderSide:
+                          BorderSide(color: Color.fromARGB(255, 24, 17, 56)),
+                    ),
+                    hintStyle: TextStyle(color: Colors.white),
+                  ),
+                  keyboardType: TextInputType.emailAddress,
+                  style: const TextStyle(color: Colors.white),
+                ),
+              ),
+              const SizedBox(
+                height: 30,
+              ),
+              SizedBox(
+                width: double.infinity,
+                child: TextField(
+                  controller: contracontrol,
+                  decoration: const InputDecoration(
+                    hintText: 'Contraseña',
+                    border: OutlineInputBorder(
+                      borderSide:
+                          BorderSide(color: Color.fromARGB(255, 71, 47, 78)),
+                    ),
+                    hintStyle: TextStyle(color: Colors.white),
+                  ),
+                  obscureText: true,
+                  style: const TextStyle(color: Colors.white),
+                ),
+              ),
+              const SizedBox(
+                height: 40,
+              ),
+              ElevatedButton(
+                onPressed: () async {
+                  if (emailcontrol.text.isEmpty || contracontrol.text.isEmpty) {
+                    ScaffoldMessenger.of(context).showSnackBar(
+                      const SnackBar(
+                          content: Text('Por favor, rellene todos los campos')),
+                    );
+                    return;
+                  }
+
+                  try {
+                    final response = await supabase.auth.signInWithPassword(
+                      email: emailcontrol.text,
+                      password: contracontrol.text,
+                    );
+
+                    if (response.user == null) {
+                      ScaffoldMessenger.of(context).showSnackBar(
+                        const SnackBar(
+                          content: Text('Error al iniciar sesión'),
+                        ),
+                      );
+                    } else {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                            builder: (context) => const menu.Menu()),
+                      );
+                    }
+                  } catch (e) {
+                    ScaffoldMessenger.of(context).showSnackBar(
+                      SnackBar(
+                        content: Text('Error al iniciar sesión: $e'),
+                      ),
+                    );
+                  }
+                },
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: const Color.fromARGB(255, 71, 47, 78),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(20),
+                  ),
+                  minimumSize: const Size(double.infinity, 50),
+                ),
+                child: const Text(
+                  'Ingresar',
+                  style: TextStyle(color: Colors.white),
+                ),
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              ElevatedButton(
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                        builder: (context) => const registro.Registro()),
+                  );
+                },
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: const Color.fromARGB(255, 71, 47, 78),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(20),
+                  ),
+                  minimumSize: const Size(double.infinity, 50),
+                ),
+                child: const Text(
+                  'Registrarte',
+                  style: TextStyle(color: Colors.white),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
--- lib/registro.dart
+++ lib/registro.dart
@@ -1,14 +1,18 @@
 import 'package:flutter/material.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
+
 final supabase = Supabase.instance.client;
 
 Future<void> main() async {
   await Supabase.initialize(
     url: 'https://qndkjfhorupharaphaoa.supabase.co',
-    anonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFuZGtqZmhvcnVwaGFyYXBoYW9hIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTI1MDQwMjEsImV4cCI6MjAyODA4MDAyMX0.Fok8bU8EcKUZig0MJZjxQZxOf6ZWnWvjwbzwmRvBU-E',
-      );
+    anonKey:
+        'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFuZGtqZmhvcnVwaGFyYXBoYW9hIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTI1MDQwMjEsImV4cCI6MjAyODA4MDAyMX0.Fok8bU8EcKUZig0MJZjxQZxOf6ZWnWvjwbzwmRvBU-E',
+  );
 
-  runApp(MaterialApp(home: Registro(),));
+  runApp(const MaterialApp(
+    home: Registro(),
+  ));
 }
 
 class Registro extends StatefulWidget {
@@ -19,59 +23,155 @@ class Registro extends StatefulWidget {
 }
 
 class _RegistroState extends State<Registro> {
-  final datos = Supabase.instance.client.from('contraseñas').select('''contraseñas,titulos,descripcion,id_usuario''');
+  final datos = Supabase.instance.client
+      .from('contraseñas')
+      .select('''contras,titulos,descripcion,id''');
   final controlregistrocorreo = TextEditingController();
   final controlregistrocontra = TextEditingController();
   final controlregistrocontrarepetir = TextEditingController();
 
   @override
-
-
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text('Registro de usuario'),),
+      appBar: AppBar(
+        title: const Text('Registro de usuario'),
+      ),
       body: Center(
-        child: FutureBuilder(future: datos, builder: (context, snapshot) {
-          if (snapshot.hasError) {
-          return Text('Error: ${snapshot.error}');
-        }
-        if (snapshot.connectionState == ConnectionState.waiting) {
-         return const CircularProgressIndicator();
-        }
-        return Column(
-          children: [
-            const SizedBox(height: 50,),
-        SizedBox(
-          width: 350,
-        child:TextField(
-          controller: controlregistrocorreo,
-          decoration: const InputDecoration(labelText: 'Ingrese su correo',border: OutlineInputBorder()),
-          keyboardType: TextInputType.emailAddress,
-        )),
-        const SizedBox(height: 30,),
-        SizedBox(
-          width: 350,
-        child: TextField(
-          controller: controlregistrocontra,
-          decoration: const InputDecoration(labelText: 'Ingrese la contraseña',border: OutlineInputBorder()),
-          obscureText: true,
-        )),
-        const SizedBox(height: 30,),
-        SizedBox(
-          width: 350,
-        child: TextField(
-          controller: controlregistrocontrarepetir,
-          decoration: const InputDecoration(labelText: 'Repita la contraseña contraseña',border: OutlineInputBorder()),
-          obscureText: true,
-        )),
-        const SizedBox(height: 40,),
-        MaterialButton(onPressed: () async {
-          //final sesion = 
-          //supabase.auth.signUp(password: contracontrol.text,email: emailcontrol.text);
-        }, child: const Text('Ingresar'),),
-          ],
-        );
-        }),
+        child: FutureBuilder(
+            future: datos,
+            builder: (context, snapshot) {
+              if (snapshot.hasError) {
+                return Text('Error: ${snapshot.error}');
+              }
+              if (snapshot.connectionState == ConnectionState.waiting) {
+                return const CircularProgressIndicator();
+              }
+              return Column(
+                children: [
+                  const SizedBox(
+                    height: 50,
+                  ),
+                  Container(
+                    margin: const EdgeInsets.symmetric(
+                        horizontal: 40, vertical: 10),
+                    child: SizedBox(
+                      width: double.infinity,
+                      child: TextField(
+                        controller: controlregistrocorreo,
+                        decoration: const InputDecoration(
+                          hintText: 'Ingrese su correo',
+                          border: OutlineInputBorder(
+                            borderSide: BorderSide(
+                                color: Color.fromARGB(255, 71, 47, 78)),
+                          ),
+                          hintStyle: TextStyle(color: Colors.white),
+                        ),
+                        keyboardType: TextInputType.emailAddress,
+                        style: const TextStyle(color: Colors.white),
+                      ),
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 30,
+                  ),
+                  Container(
+                    margin: const EdgeInsets.symmetric(
+                        horizontal: 40, vertical: 10),
+                    child: SizedBox(
+                      width: double.infinity,
+                      child: TextField(
+                        controller: controlregistrocontra,
+                        decoration: const InputDecoration(
+                          hintText: 'Ingrese la contraseña',
+                          border: OutlineInputBorder(
+                            borderSide: BorderSide(
+                                color: Color.fromARGB(255, 71, 47, 78)),
+                          ),
+                          hintStyle: TextStyle(color: Colors.white),
+                        ),
+                        obscureText: true,
+                        style: const TextStyle(color: Colors.white),
+                      ),
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 30,
+                  ),
+                  Container(
+                    margin: const EdgeInsets.symmetric(
+                        horizontal: 40, vertical: 10),
+                    child: SizedBox(
+                      width: double.infinity,
+                      child: TextField(
+                        controller: controlregistrocontrarepetir,
+                        decoration: const InputDecoration(
+                          hintText: 'Repita la contraseña',
+                          border: OutlineInputBorder(
+                            borderSide: BorderSide(
+                                color: Color.fromARGB(255, 71, 47, 78)),
+                          ),
+                          hintStyle: TextStyle(color: Colors.white),
+                        ),
+                        obscureText: true,
+                        style: const TextStyle(color: Colors.white),
+                      ),
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 40,
+                  ),
+                  Container(
+                    margin: const EdgeInsets.symmetric(
+                        horizontal: 40, vertical: 10),
+                    child: MaterialButton(
+                      onPressed: () async {
+                        final email = controlregistrocorreo.text;
+                        final password = controlregistrocontra.text;
+                        final passwordRepeat =
+                            controlregistrocontrarepetir.text;
+
+                        if (password != passwordRepeat) {
+                          ScaffoldMessenger.of(context).showSnackBar(
+                            const SnackBar(
+                              content: Text('Las contraseñas no coinciden'),
+                            ),
+                          );
+                          return;
+                        }
+
+                        try {
+                          // Primero, registra al usuario
+                          await supabase.auth
+                              .signUp(email: email, password: password);
+
+                          // Luego, inicia sesión con las mismas credenciales
+                          await supabase.auth.signInWithPassword(
+                              email: email, password: password);
+
+                          // Aquí puedes navegar a la página principal de tu aplicación
+                        } catch (error) {
+                          ScaffoldMessenger.of(context).showSnackBar(
+                            SnackBar(
+                              content: Text('Error al registrar: $error'),
+                            ),
+                          );
+                        }
+                      },
+                      color: const Color.fromARGB(255, 71, 47, 78),
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(20),
+                      ),
+                      minWidth: double.infinity,
+                      height: 50,
+                      child: const Text(
+                        'Ingresar',
+                        style: TextStyle(color: Colors.white),
+                      ),
+                    ),
+                  )
+                ],
+              );
+            }),
       ),
     );
   }
